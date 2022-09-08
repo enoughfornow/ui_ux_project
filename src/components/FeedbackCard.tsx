7ea0fc046f4549bab0b420d83ef1236e16dc5fad
Dmitry <enoughfornowbusiness@gmail.com>
2022-09-08 23:21:24 +0300

    building done!

--- src/components/FeedbackCard.tsx
+++ src/components/FeedbackCard.tsx
@@ -1,5 +1,6 @@
 import { quotes } from '../assets';
 
+
 export const FeedbackCard = ({ content, name, title, img }) => (
   <div
     className="flex justify-between flex-col px-10 py-12 rounded-[20px] 
@@ -8,5 +9,12 @@ export const FeedbackCard = ({ content, name, title, img }) => (
     <p className="font-poppins font-normal text-[18px] leading-[32px] text-white my-10">
       {content}
     </p>
+    <div className="flex flex-row">
+      <img src={img} alt={name} className="w-[48px] h-[48px] rounded-full" />
+      <div className="flex flex-col ml-4">
+        <h4 className="font-poppins font-semibold text-[20px] leading-[32px] text-white">{name}</h4>
+        <p className="font-poppins font-semibold text-[16px] leading-[24px] text-dimWhite">{title}</p>
+      </div>
+    </div>
   </div>
 );
